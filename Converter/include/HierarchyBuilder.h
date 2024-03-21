22a93b40686573fc9ed39df9a15aa017ff385695
CSCHURMANN-Legeneering <105835339+CSCHURMANN-Legeneering@users.noreply.github.com>
2024-03-21 09:51:17 +0800

    Update HierarchyBuilder.h

--- Converter/include/HierarchyBuilder.h
+++ Converter/include/HierarchyBuilder.h
@@ -344,9 +344,10 @@ struct HierarchyBuilder{
 		}
 
 		// redundant security check
-		// if(iEndsWith(this->path, ".hierarchyChunks")){
-		// 	fs::remove_all(this->path);
-		// }
+		if(iEndsWith(this->path, ".hierarchyChunks")){
+			cout << "IM IN THE REMOVE ALL CHECK" << endl;
+			fs::remove_all(this->path);
+		}
 
 		return;
 	}
